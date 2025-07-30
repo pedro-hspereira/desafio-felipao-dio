35031b1a841b78c8c04cfec6f599c81e94d37c9d
Pedro Henrique <pedro.hsp2710@gmail.com>
2025-07-30 20:44:20 -0300

    mudança do código

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("Ola mundo hehe lol pog eh o manikas")
+let personagensENivel = [
+    ["Link", 7400],
+    ["Pikachu", 9100],
+    ["Mario", 4300],
+    ["Sukuna", 12000]
+];
+
+for (let i = 0; i < personagensENivel.length; i++) {
+    let nivel = personagensENivel[i][1];
+    let nome = personagensENivel[i][0];
+
+    if (nivel < 1000) {
+        nivel = "Ferro"
+    } else if (nivel >= 1001 && nivel <= 2000) {
+        nivel = "Bronze"
+    } else if (nivel >= 2001 && nivel <= 5000) {
+        nivel = "Prata"
+    } else if (nivel >= 5001 && nivel <= 7000) {
+        nivel = "Ouro"
+    } else if (nivel >= 7001 && nivel <= 8000) {
+        nivel = "Platina"
+    } else if (nivel >= 8001 && nivel <= 9000) {
+        nivel = "Ascendente"
+    } else if (nivel >= 9001 && nivel <= 10000) {
+        nivel = "Imortal"
+    } else if (nivel >= 10001) {
+        nivel = "Radiante"
+    }
+    console.log(`O personagem ${nome} está no elo ${nivel}.`)
+}
